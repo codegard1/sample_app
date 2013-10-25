4bf871244da57b470340964000649e3d376bdba5
codegard1 <c.odegard@gmail.com>
2013-10-24 20:34:43 -0400

    added contact page & rspec for the same

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,41 +5,54 @@ describe "Static Pages" do
 
 	describe "Home Page" do 
 		it "should have the content 'Sample App'" do
-			visit '/static_pages/home' 
+			visit root_path 
 			expect(page).to have_content('Sample App')
 		end
 		it "Should have the base title 'Sample App'" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).to have_title('Sample App')
 		end
 
 		it "should not have a custom page title" do
-			visit 'static_pages/home'
+			visit root_path
 			expect(page).not_to have_title('Home')
 		end
 	end	
 
 	describe "Help Page" do
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_content('Help')
 		end
 		it "should have the title 'Sample App | Help'" do
-			visit 'static_pages/help'
+			visit help_path
 			expect(page).to have_title('Sample App | Help')
 		end
 	end
 
 	describe "About Page" do
 		it "should have the content 'About Us'." do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_content('About Us')
 		end
 		it "should have the title 'Sample App | About Us'" do
-			visit 'static_pages/about'
+			visit about_path
 			expect(page).to have_title('Sample App | About')
 		end
 	end
+
+	describe "Contact page" do
+
+		it "should have the content 'Contact'" do
+			visit contact_path
+			expect(page).to have_content('Contact')
+		end
+		it "should have the title 'Contact'" do
+			visit contact_path
+			expect(page).to have_title('Sample App | Content')
+		end
+	end
 end
+
 
 
