e8a6acc19cbfddd85ed4b823b3ddf2c8e8be7fca
codegard1 <c.odegard@gmail.com>
2013-11-02 12:46:13 -0400

    Added signup page. Finished Ch5 of tutorial.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 
 describe "User pages" do
 
+	subject { page }
+
+	describe "signup page" do
+		before { visit signup_path }
+
+		it { should have_content( 'Sign Up') }
+		it { should have_title( 'Sign Up') }
+	end
+end
+
+
+
 	
