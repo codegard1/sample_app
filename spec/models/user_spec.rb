24a8e74099872bdd77874fc000c50fd7b5d7bb03
codegard1 <c.odegard@gmail.com>
2013-11-09 17:19:37 -0500

    validation tests for user name and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 describe User do
 
-		before { @user=User.new(name:"Chris Odegard",email:"c.odegard@gmail.com") }
+	before { @user=User.new(name:"Chris Odegard",email:"c.odegard@gmail.com") }
 
-		subject { @user }
+	
+	subject { @user }
 
-		it { should respond_to(:name) }
 		it { should respond_to(:email) }
+		it { should respond_to(:name) }
+
+		it { should be_valid }
+
+		describe "when name is not present" do
+			before { @user.name = " " }
+			it { should_not be_valid }
+		end
+		describe "when email is not present" do
+			before {@user.email = " " }
+			it { should_not be_valid }
+		end
 end
